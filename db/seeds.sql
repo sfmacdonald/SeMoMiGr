0e8f6dd6c529c54fddc75a5d36d339276d4938a1
Sean MacDonald <147445255+sfmacdonald@users.noreply.github.com>
2024-08-16 18:35:23 -0700

    "Added registry page and updated navigation, styles, and database seeds"

--- db/seeds.sql
+++ db/seeds.sql
@@ -2,8 +2,8 @@ USE WeddingDB;
 
 -- Insert attendees
 INSERT INTO attendees (name, email, party_size, other_details) VALUES
-('Sean MacDonald', 'seanfmacdonald@icloud.com', 4, 'Groom'),
-('Scott MacDonald', , 2, 'Father of Groom'),
+
+('Scott MacDonald', 'scott@akwater.com', 2, 'Father of Groom'),
 ('Kelley MacDonald', 'kelley_vt78@hotmail.com', 2, 'Mother of Groom'),
 ('Katherine Colquhoun', 'katherine.colquhoun@gmail.com', 3, 'Sister of Groom'),
 ('Jason Colquhoun', 'jason@designalaska.com', 3, 'BIL of Groom'),
@@ -14,21 +14,23 @@ INSERT INTO attendees (name, email, party_size, other_details) VALUES
 ('June Bunting', 'junecarterbunting@gmail.com', 2, 'Stepmother of Bride'),
 ('Brian Bunting', 'brianbunting@gmail.com', 2, 'Brother of Bride'),
 ('Missy Bunting', 'misserb82@yahoo.com', 2, 'Sister of Bride'),
-('Ann-Clay Brough' '', 4, 'Friend of Bride & Groom'),
-('Greg David', '', 3, 'Friend of Groom'),
+('Ann-Clay Brough', 'null1', 4, 'Friend of Bride & Groom'),
+('Greg David', 'null2', 3, 'Friend of Groom'),
 ('Stuart Wright', 'sfwbuilder@gmail.com', 2, 'Uncle of Groom'),
 ('Larry Voigtsberger', 'Lvdesign@mac.com', 2, 'Uncle of Groom'),
 ('Carrie Lorenzo', 'Lorenzoc1027@gmail.com', 2, 'Aunt of Groom'),
-('Meg Bowser', '', 4, 'Aunt of Groom'),
-('Ellen Wright', '', 1, 'Grandmother of Groom'),
+('Meg Bowser', 'null3', 4, 'Aunt of Groom'),
+('Ellen Wright', 'null4', 1, 'Grandmother of Groom'),
 ('Jeff Marchese', 'jrdlfamily@gmail.com', 4, 'Friend of Bride & Groom'),
 ('Gary Rapanotti', 'surveyor@vermontel.net', 2, 'Friend of the Groom'),
-('Abbey MacDonald', '', 1, 'Daughter of Groom'),
-('Mary Jane Kolenda', '', 4, 'Cousin of Groom'),
-('Bill DeVoe', '', 1, 'Grandfather of Groom'),
-('Christina Smith', '', 5, 'Cousin of Groom');
+('Abbey MacDonald', 'null5', 1, 'Daughter of Groom'),
+('Mary Jane Kolenda', 'null6', 4, 'Cousin of Groom'),
+('Bill DeVoe', 'null7', 1, 'Grandfather of Groom'),
+('Christina Smith', 'null8', 5, 'Cousin of Groom');
 
 -- Insert RSVPs
 INSERT INTO rsvps (attendee_id, response) VALUES
 (1, 'Yes'),
 (2, 'Yes');
+
+('Sean MacDonald', 'seanfmacdonald@icloud.com', 4, 'Groom'),
